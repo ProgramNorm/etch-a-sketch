f62161b60c53ba4536f22245730edfe7c6d71eb6
ProgramNorm <beatsbynorm@gmail.com>
2017-07-19 12:16:04 -0400

    Add more user interface. Must complete clear function.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
+	createGrid();
+	$('.grid').hover(function(){
+		$(this).css('background-color', 'black');
+	});
+	$('.clear').on('click', function() {
+		$('.container').empty();
+		createGrid();
+	});
+});
+
+function createGrid(ui) {       
 	var ui = prompt('Choose the size of the grid (1-100 for best performance)');
-	if (ui < 4 || typeof(ui) !== 'number'){
+	if (ui < 4 || typeof(parseInt(ui)) !== 'number'){
 		ui = 4;
-		alert('Default minimum of 4 was chosen.');
+		alert('Default minimum of 4 was chosen');
 	} else if(ui > 100) {
 		ui = 100;
 		alert('Default maximum of 100 was chosen');
 	}
-	createGrid(ui);
-	$('.grid').hover(function(){
-		$(this).css('background-color', 'black');
-	});
-});
 
-function createGrid(ui) {          
 	for(var i = 0; i < ui*ui; i++){
 		$('.container').append('<div class="grid" style="height:'+100/ui+'%; width:'+100/ui+'%"></div>')
 	}
